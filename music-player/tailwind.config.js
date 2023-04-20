dac123759c60b8d288d6d420841edef30ea0e8c6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-04-20 18:33:40 -0300

    chore(music-player): add app colors

--- music-player/tailwind.config.js
+++ music-player/tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
       },
+      colors: {
+        purple: {
+          950: '#0F0D13',
+          900: '#2A2141',
+        },
+        gray: {
+          50: '#E1E1E6',
+          100: '#C4C4CC',
+          200: '#D9D9D9',
+        },
+      },
     },
   },
   plugins: [],
