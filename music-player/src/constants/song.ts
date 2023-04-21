47b917e8ddf23864932bc5e8b8eb8861c4cf125e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-04-20 21:01:54 -0300

    chore(music-player): add song contants

---
+++ music-player/src/constants/song.ts
@@ -0,0 +1,6 @@
+export const SONG = {
+  name: 'Polímero Biológico',
+  author: 'Georges Stavracas',
+  audioPath: '/feaneron_polimero_biologico.mp3',
+  thumbnailPath: '/polimero_biologico.png',
+}
