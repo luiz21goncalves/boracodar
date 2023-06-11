39d47e441e02c19523e4064e4661ab7c1ba50acd
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-11 13:02:12 -0300

    feat(product-card): toggle between image and gif

--- product-card/src/pages/Home/components/Asset/index.tsx
+++ product-card/src/pages/Home/components/Asset/index.tsx
@@ -1,10 +1,29 @@
-import sofa from '../../../../assets/sofa-margot-ii-rose.png'
+import { useState } from 'react'
+
+import { ArrowsClockwise, X } from '@phosphor-icons/react'
+
+import sofaGif from '../../../../assets/sofa-margot-ii-rose.gif'
+import sofaImage from '../../../../assets/sofa-margot-ii-rose.png'
 import style from './style.module.css'
 
 export function Asset() {
+  const [isShowingAnimation, setIsShowingAnimation] = useState(false)
+
+  const imageSrc = isShowingAnimation ? sofaGif : sofaImage
+
+  const Icon = isShowingAnimation ? X : ArrowsClockwise
+
+  function handleToggle() {
+    setIsShowingAnimation((prevState) => !prevState)
+  }
+
   return (
     <section className={style.container}>
-      <img src={sofa} alt="sofa" />
+      <img src={imageSrc} alt="Sofá Margot II - Rosé" />
+
+      <button className={style['float-button']} onClick={handleToggle}>
+        <Icon size={32} />
+      </button>
     </section>
   )
 }
