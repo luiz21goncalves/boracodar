45e6c35f7c41471db1c0238874ef596b5ede6d15
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-11 10:32:51 -0300

    style(product-card): add rocketseat style guide

--- product-card/.eslintrc.cjs
+++ product-card/.eslintrc.cjs
@@ -4,11 +4,24 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-import-helpers'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/self-closing-comp': 'error',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['/^react/', 'module', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
   },
 }
